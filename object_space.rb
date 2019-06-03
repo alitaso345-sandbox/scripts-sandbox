1f0684f35aa6b5ce9e31060b502c4f14bf65180d
alitaso345 <alice.maru345@gmail.com>
2019-06-03 22:39:42 +0900

    memory profiler

--- object_space.rb
+++ object_space.rb
@@ -1,10 +1,21 @@
 require 'objspace'
 
-ObjectSpace.memsize_of({a: 4})
-#=> 232
+ObjectSpace.trace_object_allocations_start
+foo = ["foo"] * 255
+ObjectSpace.trace_object_allocations_stop
 
-ObjectSpace.memsize_of(123)
-#=> 0
+trace_file = File.basename(__FILE__) # object_space.rb
+ObjectSpace.each_object do |o|
+  file = ObjectSpace.allocation_sourcefile(o)
+  next unless file == trace_file # 今回は本ファイルのみを追う
+  line = ObjectSpace.allocation_sourceline(o)
+  memsize = ObjectSpace.memsize_of(o)
+  klass = o.class
+  puts "#{memsize} #{file}:#{line}:#{klass}"
+end
+ObjectSpace.trace_object_allocations_clear
 
-ObjectSpace.memsize_of('123')
-#=> 40
+#=>
+# 2080 object_space.rb:4:Array
+# 40 object_space.rb:4:Array
+# 40 object_space.rb:4:String
