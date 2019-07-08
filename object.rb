22f364cf86f38227cbe990b2f7b7aac1cec370fb
alitaso345 <alice.maru345@gmail.com>
2019-07-08 23:34:15 +0900

    hash and method

--- object.rb
+++ object.rb
@@ -1,14 +1,19 @@
 class Foo
-  def foo(arg)
-    "foo called with arg #{arg}"
-  end
+  def foo() "foo" end
+  def bar() "bar" end
+  def baz() "baz" end
 end
 
-m = Foo.new.method(:foo)
-p m
-p m.call(1)
+obj = Foo.new
 
-pr = Proc.new {|arg| "proc called with arg #{arg}"}
-p pr
-p pr.call(1)
+# 任意のキーとメソッドの関係をハッシュに保持しておく
+methods = {
+  1 => obj.method(:foo),
+  2 => obj.method(:bar),
+  3 => obj.method(:baz)
+}
 
+# キーを使って関連するメソッドを呼び出す
+p methods[1].call
+p methods[2].call
+p methods[3].call
