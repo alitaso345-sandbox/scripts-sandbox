2b9e7a97ad1b52e6e2ca65f1db9874c9f045686a
alitaso345 <alice.maru345@gmail.com>
2019-07-08 23:26:02 +0900

    method and proc

--- object.rb
+++ object.rb
@@ -1,5 +1,14 @@
-me = -365.method(:abs)
-p me #=> Integerオブジェクトのメソッド、Integer#absをオブジェクト化した Method オブジェクトを返す
-p me.call
+class Foo
+  def foo(arg)
+    "foo called with arg #{arg}"
+  end
+end
 
+m = Foo.new.method(:foo)
+p m
+p m.call(1)
+
+pr = Proc.new {|arg| "proc called with arg #{arg}"}
+p pr
+p pr.call(1)
 
