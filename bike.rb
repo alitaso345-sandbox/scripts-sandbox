90a10d1885601f4cbb51abea0aafd727503449c0
alitaso345 <alice.maru345@gmail.com>
2019-07-19 07:35:53 +0900

    schedule class

--- bike.rb
+++ bike.rb
@@ -1,12 +1,38 @@
+class Schedule
+  def scheduled?(schedulable, start_date, end_date)
+    puts "This #{schedulable.class}" + "is not scheduled" + " between #{start_date} and #{end_date}"
+  end
+
+  def add(target, starting, ending)
+
+  end
+
+  def remove(target, starting, ending)
+
+  end
+end
 class Bicycle
-  attr_reader :size, :chain, :tire_size
+  attr_reader :schedule, :size, :chain, :tire_size
 
   def initialize(args={})
+    @schedule = args[:schedule] || Schedule.new
     @size = args[:size]
     @chain = args[:chain] || default_chain
     @tire_size = args[:tire_size] || default_tire_size
 
     post_initialize(args)
+  end
+
+  def schedulable?(start_date, end_date)
+    !scheduled?(start_date - lead_days, end_date)
+  end
+
+  def scheduled?(start_date, end_date)
+    schedule.scheduled?(self, start_date, end_date)
+  end
+
+  def lead_days
+    1
   end
 
   def post_initialize(args)
@@ -86,14 +112,10 @@ class RecumbentBike < Bicycle
   end
 end
 
+require 'date'
 
-road_bike = RoadBike.new(size: 'M', tape_color: 'red')
-p road_bike.tire_size
-p road_bike.chain
+starting = Date.parse('2015/9/4')
+ending = Date.parse('2015/9/10')
 
-mountain_bike = MountainBike.new(size: 'S', front_shock: 'Manitou', rear_shock: 'Fox')
-p mountain_bike.tire_size
-p mountain_bike.chain
-
-bent = RecumbentBike.new(flag: 'tall and orange')
-p bent.spares
+b = Bicycle.new(tire_size: '23')
+b.schedulable?(starting, ending)
