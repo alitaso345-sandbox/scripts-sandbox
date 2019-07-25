045a2684068e119dda1b060e22f99fa35f2e9350
alitaso345 <alice.maru345@gmail.com>
2019-07-26 05:20:35 +0900

    Parts class

--- bike.rb
+++ bike.rb
@@ -1,57 +1,10 @@
-class Schedule
-  def scheduled?(schedulable, start_date, end_date)
-    puts "This #{schedulable.class}" + "is not scheduled" + " between #{start_date} and #{end_date}"
-  end
-
-  def add(target, starting, ending)
-
-  end
-
-  def remove(target, starting, ending)
-
-  end
-end
-
-module Schedulable
-  attr_writer :schedule
-
-  def schedule
-    @schedule ||= ::Schedule.new
-  end
-
-  def schedulable?(start_date, end_date)
-    !scheduled?(start_date - lead_days, end_date)
-  end
-
-  def scheduled?(start_date, end_date)
-    schedule.scheduled?(self, start_date, end_date)
-  end
-
-  def lead_days
-    0
-  end
-end
-
-class Bicycle
-  include Schedulable
-
-  attr_reader :size, :chain, :tire_size
+class Parts
+  attr_reader :chain, :tire_size
 
   def initialize(args={})
-    @schedule = args[:schedule] || Schedule.new
-    @size = args[:size]
     @chain = args[:chain] || default_chain
     @tire_size = args[:tire_size] || default_tire_size
-
     post_initialize(args)
-  end
-
-  def lead_days
-    1
-  end
-
-  def post_initialize(args)
-    nil
   end
 
   def spares
@@ -61,6 +14,15 @@ class Bicycle
     }.merge(local_spares)
   end
 
+  def default_tire_size
+    raise NotImplementedError
+  end
+
+  # subclasses may override
+  def post_initialize(args)
+    nil
+  end
+
   def local_spares
     {}
   end
@@ -68,21 +30,17 @@ class Bicycle
   def default_chain
     '10-speed'
   end
-
-  def default_tire_size
-    raise NotImplementedError, "This #{self.class} cannot respond to:"
-  end
 end
 
-class RoadBike < Bicycle
- attr_reader :tape_color
+class RoadBikeParts < Parts
+  attr_reader :tape_color
 
   def post_initialize(args)
     @tape_color = args[:tape_color]
   end
 
   def local_spares
-    { tape_color: tape_color}
+    {tape_color: tape_color}
   end
 
   def default_tire_size
@@ -90,7 +48,7 @@ class RoadBike < Bicycle
   end
 end
 
-class MountainBike < Bicycle
+class MountainBikeParts < Parts
   attr_reader :front_shock, :rear_shock
 
   def post_initialize(args)
@@ -107,52 +65,23 @@ class MountainBike < Bicycle
   end
 end
 
-class RecumbentBike < Bicycle
-  attr_reader :flag
+class Bicycle
+  attr_reader :size, :parts
 
-  def post_initialize(args)
-    @flag = args[:flag]
+  def initialize(args={})
+    @size = args[:size]
+    @parts = args[:parts]
   end
 
-  def local_spares
-    {flag: flag}
-  end
-
-  def default_chain
-    '9-speed'
-  end
-
-  def default_tire_size
-    '28'
+  def spares
+    parts.spares
   end
 end
 
-class Vehicle
-  include Schedulable
+road_bike = Bicycle.new(size: 'L', parts: RoadBikeParts.new(tape_color: 'red'))
+pp road_bike.size
+pp road_bike.spares
 
-  def lead_days
-    3
-  end
-end
-
-class Mechanic
-  include Schedulable
-
-  def lead_days
-    4
-  end
-end
-
-require 'date'
-
-starting = Date.parse('2015/9/4')
-ending = Date.parse('2015/9/10')
-
-b = Bicycle.new(tire_size: '23')
-b.schedulable?(starting, ending)
-
-v = Vehicle.new
-v.schedulable?(starting, ending)
-
-m = Mechanic.new
-m.schedulable?(starting, ending)
+mountain_bike = Bicycle.new(size: 'L', parts: MountainBikeParts.new(rear_shock: 'Fox'))
+pp mountain_bike.size
+pp mountain_bike.spares
