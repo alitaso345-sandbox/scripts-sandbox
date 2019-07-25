4c35d22de93250e67444ad981b716f9bfc64a570
alitaso345 <alice.maru345@gmail.com>
2019-07-26 05:59:14 +0900

    Part object

--- bike.rb
+++ bike.rb
@@ -1,34 +1,28 @@
-class Parts
-  attr_reader :chain, :tire_size
+require 'forwardable'
 
-  def initialize(args={})
-    @chain = args[:chain] || default_chain
-    @tire_size = args[:tire_size] || default_tire_size
-    post_initialize(args)
+class Parts
+  extend Forwardable
+  def_delegators :@parts, :size, :each
+  include Enumerable
+
+  attr_reader :parts
+
+  def initialize(parts)
+    @parts = parts
   end
 
   def spares
-    {
-      tire_size: tire_size,
-      chain: chain
-    }.merge(local_spares)
+    parts.select {|part| part.needs_spare}
   end
+end
 
-  def default_tire_size
-    raise NotImplementedError
-  end
+class Part
+  attr_reader :name, :description, :needs_spare
 
-  # subclasses may override
-  def post_initialize(args)
-    nil
-  end
-
-  def local_spares
-    {}
-  end
-
-  def default_chain
-    '10-speed'
+  def initialize(args)
+    @name = args[:name]
+    @description = args[:description]
+    @needs_spare = args.fetch(:needs_spare, true)
   end
 end
 
@@ -78,10 +72,20 @@ class Bicycle
   end
 end
 
-road_bike = Bicycle.new(size: 'L', parts: RoadBikeParts.new(tape_color: 'red'))
+chain = Part.new(name: 'chain', description: '10-speed')
+road_tire = Part.new(name: 'tire_size', description: '23')
+tape = Part.new(name: 'tape_color', description: 'red')
+
+road_bike = Bicycle.new(size: 'L', parts: Parts.new([chain, road_tire, tape]))
 pp road_bike.size
 pp road_bike.spares
 
-mountain_bike = Bicycle.new(size: 'L', parts: MountainBikeParts.new(rear_shock: 'Fox'))
+mountain_tire = Part.new(name: 'tire_sieze', description: '2.1')
+rear_shock = Part.new(name: 'rear_shock', description: 'Fox')
+front_shock = Part.new(name: 'front_shock', description: 'Manitou', needs_spare: false)
+
+mountain_bike = Bicycle.new(size: 'L', parts: Parts.new([chain, mountain_tire, front_shock, rear_shock]))
 pp mountain_bike.size
 pp mountain_bike.spares
+pp mountain_bike.spares.size
+pp mountain_bike.parts.size
